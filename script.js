c185614cdb127b4c48bb33712f9c94fad45f28e7
SyedShozabZaidi <shozthegreat@gmail.com>
2023-02-07 03:13:13 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+//          For Loop Practice            //
+
+// // for (let i = 0; i < 40; i++){
+// //     console.log(i)
+// // } 
+
+// for(a = 0; a < 35; a++){
+//     document.write(a)
+// }
+
+// let n = prompt("Enter your value")
+// let sum = 0
+// n = Number.parseInt(n)
+ 
+// for(i = 0; i < n; i++){
+
+//     sum += (i + 1)
+//     console.log((i+1) + "+")
+// }
+
+// console.log(sum)
+
+//              For In Loop             //
+
+// let gpu = {
+//     RTX_3090ti: "2000$",
+//     RTX_4090ti: "2599$",
+//     RTX_3070ti: "599$",
+//     RTX_3080ti   : "1299$" 
+// }
+
+// for (let a in gpu){
+//     console.log("Model of " + a + " is " + gpu[a])
+// }
+
+
+//          For Of Loop         //
+
+for (let a of "Rtx3090ti"){
+    console.log(a)
+}
